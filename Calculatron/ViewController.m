4d8c2e006cb724e20850ab0fca32970419f0f1f6
Pei Xiong <peixiong@Peis-MacBook-Pro.local>
2016-03-17 14:48:20 -0700

    six commit

--- Calculatron/ViewController.m
+++ Calculatron/ViewController.m
@@ -25,10 +25,20 @@
 
 }
 - (IBAction)onOperationTap:(UIButton *)sender {
+    self.operationLabel.text = sender.titleLabel.text;
+    
     double op1 = self.operand1.text.doubleValue;
     double op2 = self.operand2.text.doubleValue;
+    
+    NSString *operation = self.operationLabel.text;
+    if ([operation isEqualToString:@"+"]) {
+        self.resultLabel.text = [NSString stringWithFormat:@"%f", op1 + op2];
+    } else if([operation isEqualToString:@"-"]){
+        self.resultLabel.text = [NSString stringWithFormat:@"%f", op1 -op2];
+    }else if([operation isEqualToString:@"x"]){
+        self.resultLabel.text = [NSString stringWithFormat:@"%f", op1*op2];
+    }else if([operation isEqualToString:@"/"]){
+        self.resultLabel.text = [NSString stringWithFormat:@"%f", op1/op2];
+    }
 }
-
-
-
 @end
